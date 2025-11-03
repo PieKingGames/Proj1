9feab6a018c7cfa72737ea6064afd59fa0ab83a9
PieKingGames <piekingbusiness@gmail.com>
2025-11-03 19:17:17 +0000

    Learned range function with for loop

---
+++ Angela Yu's Coding Courses/100 Days of Python/rangeFunc (OneToOneHundred).py
@@ -0,0 +1,19 @@
+# Range Function Usage:
+# In this example is used with the for loop.
+
+for number in range(1, 11):
+
+# Processes each number from 1 to 10, excluding 11.
+    print(number)
+
+# By default, it increments by 1. Can specify as shown below:
+
+for number in range(1, 11, 3):
+    print(number)
+
+# How could we add up all the numbers from 1 to 100?
+
+total = 0 # To initialize the final sum variable.
+for i in range(1, 101):
+    total += i
+print(total)
